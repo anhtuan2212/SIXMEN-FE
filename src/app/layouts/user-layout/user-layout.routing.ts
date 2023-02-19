4e5afe21ae4519e18e8161574a762a8cb32bce95
anhtuan2212 <lutan2212@gmail.com>
2023-02-19 15:30:46 +0700

    ban ngay 18.2

--- src/app/layouts/user-layout/user-layout.routing.ts
+++ src/app/layouts/user-layout/user-layout.routing.ts
@@ -1,6 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "src/app/pages/Home/Home.component";
+import { ContactComponent } from "src/app/pages/contact/contact.component";
+import { ProductDetailComponent } from "src/app/pages/product-detail/product-detail.component";
+import { ShopComponent } from "src/app/pages/shop/shop.component";
 
 export const UserLayoutRoutes: Routes = [
   { path: "", component: HomeComponent },
+  { path: "product-detail", component: ProductDetailComponent },
+  { path: "shop", component: ShopComponent },
+  { path: "contact", component: ContactComponent },
 ];
